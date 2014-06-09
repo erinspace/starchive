dd1964187e7d1c32545664c6a4c1a09736ff48e1
erinbspace <erin.braswell@gmail.com>
2014-06-09 19:48:37 -0400

    scraper now works hell yessss

--- MicroObs_scraper.py
+++ MicroObs_scraper.py
@@ -9,10 +9,8 @@ text = url.text
 
 soup = BeautifulSoup(text)
 
+# everything is contained in the <pre> tag
 tag = soup.pre
-
-# this is all the a tags in the pre tag 
-tag.find_all('a')
 
 # This finds all the tags with the text in them we want
 all_a = [b.find('a', text=True) for b in tag.findAll('b')]
@@ -25,6 +23,12 @@ for filename in all_a:
 file_names = [ext.replace('GIF','FITS') for ext in file_names]
 
 print file_names
+print file_names
 
+base_url = 'http://mo-www.harvard.edu/ImageDirectory/'
 
-# request url = http://mo-www.harvard.edu/ImageDirectory/CentaurusA140609034607.FITS
+for filename in file_names[1:3]:
+    filename = filename.strip()
+    FITs_file = requests.get(base_url + filename)
+    with open(filename, "wb") as newfile:
+        newfile.write(FITs_file.content)
