cc4f93c53e2f6535f7b35a80d3fce1d5f350f471
erinbspace <erin.braswell@gmail.com>
2014-06-09 20:44:36 -0400

    prints so that FITS headers do not look terrible, boom sauce.

--- MicroObs_scraper.py
+++ MicroObs_scraper.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+from astropy.io import fits
 
 url = requests.get('http://mo-www.harvard.edu/jsp/servlet/MO.ID.ImageDirectory')
 
@@ -22,15 +23,25 @@ for filename in all_a:
 
 file_names = [ext.replace('GIF','FITS') for ext in file_names]
 
-print file_names
-print file_names
+# print file_names
+# print file_names
 
 base_url = 'http://mo-www.harvard.edu/ImageDirectory/'
 
-sdjf
+local_file_names = []
 
 for filename in file_names[1:3]:
     filename = filename.strip()
     FITs_file = requests.get(base_url + filename)
     with open(filename, "wb") as newfile:
         newfile.write(FITs_file.content)
+        local_file_names.append(filename)
+
+for filename in local_file_names:
+    FITS_file = fits.open(filename)
+    scidata =  FITS_file[0].header
+
+    ## To print out the FITS header nicely
+    for thingy in scidata:
+        if thingy:
+            print thingy, '=', scidata[thingy]
