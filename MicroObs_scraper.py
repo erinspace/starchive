b7cd1fb00249031085f7d8f47ec4b14f36704cbf
erinbspace <erin.braswell@gmail.com>
2014-06-09 20:56:45 -0400

    made the scraper only the scraper

--- MicroObs_scraper.py
+++ MicroObs_scraper.py
@@ -5,6 +5,8 @@ import requests
 from astropy.io import fits
 
 url = requests.get('http://mo-www.harvard.edu/jsp/servlet/MO.ID.ImageDirectory')
+base_url = 'http://mo-www.harvard.edu/ImageDirectory/'
+directory = './raw_FITS/'
 
 text = url.text
 
@@ -23,25 +25,9 @@ for filename in all_a:
 
 file_names = [ext.replace('GIF','FITS') for ext in file_names]
 
-# print file_names
-# print file_names
-
-base_url = 'http://mo-www.harvard.edu/ImageDirectory/'
-
-local_file_names = []
-
-for filename in file_names[1:3]:
+for filename in file_names[1:4]:
     filename = filename.strip()
+
     FITs_file = requests.get(base_url + filename)
-    with open(filename, "wb") as newfile:
+    with open(directory+filename, "wb") as newfile:
         newfile.write(FITs_file.content)
-        local_file_names.append(filename)
-
-for filename in local_file_names:
-    FITS_file = fits.open(filename)
-    scidata =  FITS_file[0].header
-
-    ## To print out the FITS header nicely
-    for thingy in scidata:
-        if thingy:
-            print thingy, '=', scidata[thingy]
